a9f780fea895f11f2ca611854b01b073673f37ec
PaulNyairema <pnyairema@gmail.com>
2015-04-01 09:14:45 +0300

    push docs

--- conf.py
+++ conf.py
@@ -118,7 +118,6 @@ todo_include_todos = True
 #html_theme = 'alabaster'
 
 
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
